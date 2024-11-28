c9070748e910b0ff8d16da352b17db565376787f
ainerug <115113340+ainerug@users.noreply.github.com>
2024-11-28 19:28:00 +0000

    updated

--- Server/DB/conn.js
+++ Server/DB/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://khaas25:1234@wlms.zjs0lk4.mongodb.net/aine-bakery").then(()=>{
+mongoose.connect("mongodb+srv://ainevinted:1234@cluster0.hwklz.mongodb.net/aine-bakery").then(()=>{
     console.log("Connected to DB");
 }).catch((e)=>{
     console.log(e);
